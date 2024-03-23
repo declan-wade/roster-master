80ddfc022bf62bb4114cecfeb0b3adee879a289c
declan-enercloud <declan@enercloud.io>
2024-03-23 15:54:02 +0800

    Added no duplicate shift flag Various fixes

--- app/roster/rosterTable.tsx
+++ app/roster/rosterTable.tsx
@@ -1,103 +1,111 @@
 "use client";
 import React from "react";
 import Table from "react-bootstrap/Table";
-import { DateTime } from "luxon";
-import { useSearchParams } from "next/navigation";
+import Card from "react-bootstrap/Card";
 import { getObjectFromStorage} from '../storageService'
+import { Col, Row } from 'react-bootstrap';
 
 interface Roster {
   [day: string]: {
-    [shift: string]: string[];
+    [shift: string]: string[]
   };
+}
+
+interface RosterCardProps {
+    date: any;
+    shifts: any[];
 }
 
 const RosterTable: React.FC = () => {
   const roster: Roster | null = getObjectFromStorage("roster-cookie");
-  const searchParams = useSearchParams();
-  const params: any = searchParams.get("startDate");
-  const startDate = DateTime.fromISO(params);
-  const formattedDate = startDate.toFormat("EEEE dd LLLL yyyy");
 
-  const formatDate = (startDate: any, dayIndex: any) => {
-    const formattedDate = DateTime.fromISO(startDate)
-      .plus({ days: dayIndex })
-      .toFormat("cccc LLLL d, yyyy");
-    return formattedDate;
-  };
+
+  function RosterCard({ date, shifts }: RosterCardProps) {
+    return (
+        <Card>
+          <Card.Header>
+            <Card.Title>{date}</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            {shifts && (
+                <div>
+                  {/* All-Day Shifts */}
+                  <Table striped bordered hover size="sm">
+                    <thead>
+                    <tr>
+                      <th colSpan={2}>🕒 All-Day Shifts</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {Object.entries(shifts).map(([shift, people]) =>
+                        !shift.startsWith("morning_") && !shift.startsWith("afternoon_") ? (
+                            <tr key={shift}>
+                              <td>{shift}</td>
+                              <td>{people.join(", ")}</td>
+                            </tr>
+                        ) : null
+                    )}
+                    </tbody>
+                  </Table>
+
+                  {/* Morning Shifts */}
+                  <Table striped bordered hover size="sm">
+                    <thead>
+                    <tr>
+                      <th colSpan={2}>☀️ Morning Shifts</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {Object.entries(shifts).map(([shift, people]) =>
+                        shift.startsWith("morning_") ? (
+                            <tr key={shift}>
+                              <td>{shift.split("_")[1]}</td>
+                              <td>{people.join(", ")}</td>
+                            </tr>
+                        ) : null
+                    )}
+                    </tbody>
+                  </Table>
+
+                  {/* Afternoon Shifts */}
+                  <Table striped bordered hover variant="dark" size="sm">
+                    <thead>
+                    <tr>
+                      <th colSpan={2}>🌙 Afternoon Shifts</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {Object.entries(shifts).map(([shift, people]) =>
+                        shift.startsWith("afternoon_") ? (
+                            <tr key={shift}>
+                              <td>{shift.split("_")[1]}</td>
+                              <td>{people.join(", ")}</td>
+                            </tr>
+                        ) : null
+                    )}
+                    </tbody>
+                  </Table>
+                </div>
+            )}
+          </Card.Body>
+        </Card>
+    )
+  }
 
   return (
       <div>
-        <div className="container mt-5">
+        <div className="container mt-3">
           {roster ? (
-              Object.entries(roster).map(([date, shifts]) => (
-                  <div key={date}>
-                    <h4>{date}</h4>
-                    <hr/>
-                    {shifts && (
-                        <div>
-                          {/* All-Day Shifts */}
-                          <Table striped bordered hover>
-                            <thead>
-                            <tr>
-                              <th colSpan={2}>🕒 All-Day Shifts</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {Object.entries(shifts).map(([shift, people]) =>
-                                !shift.startsWith("morning_") && !shift.startsWith("afternoon_") ? (
-                                    <tr key={shift}>
-                                      <td>{shift}</td>
-                                      <td>{people.join(", ")}</td>
-                                    </tr>
-                                ) : null
-                            )}
-                            </tbody>
-                          </Table>
-                          <br/>
-
-                          {/* Morning Shifts */}
-                          <Table striped bordered hover>
-                            <thead>
-                            <tr>
-                              <th colSpan={2}>☀️ Morning Shifts</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {Object.entries(shifts).map(([shift, people]) =>
-                                shift.startsWith("morning_") ? (
-                                    <tr key={shift}>
-                                      <td>{shift.split("_")[1]}</td>
-                                      <td>{people.join(", ")}</td>
-                                    </tr>
-                                ) : null
-                            )}
-                            </tbody>
-                          </Table>
-
-                          {/* Afternoon Shifts */}
-                          <Table striped bordered hover variant="dark">
-                            <thead>
-                            <tr>
-                              <th colSpan={2}>🌙 Afternoon Shifts</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {Object.entries(shifts).map(([shift, people]) =>
-                                shift.startsWith("afternoon_") ? (
-                                    <tr key={shift}>
-                                      <td>{shift.split("_")[1]}</td>
-                                      <td>{people.join(", ")}</td>
-                                    </tr>
-                                ) : null
-                            )}
-                            </tbody>
-                          </Table>
-                          <br/>
-                        </div>
-                    )}
-                  </div>
-              ))
-          ) : (
+            <div>
+              <Row xs={1} md={3} lg={3} className="mb-3">
+                {Object.entries(roster).map(([date, shifts]) => (
+                    <Col key={date} className="mb-3">
+                      <RosterCard date={date} shifts={shifts as any} />
+                    </Col>
+                ))}
+              </Row>
+              </div>
+              ) : (
               <h3>Loading...</h3>
           )}
         </div>
